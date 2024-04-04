8191d795eba12df8a34b4f4369fa897850d0b0ff
Anlx2000 <Anlx2000@163.com>
2024-04-04 17:59:50 +0800

    feature2

--- main.cpp
+++ main.cpp
@@ -16,17 +16,5 @@ void task(int a, int b, int& ret)
 int main()
 {
 
-    ProducerConsumer<int> producerconsumer(200, 5, 5);
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-
-    int taskNum = 20;
-    TinyThreadPool threadPool;
-    std::vector<int> result(taskNum, 0);
-    for(int i = 0; i < taskNum; i++){
-        threadPool.addTask(task,i, i + 1, std::ref(result[i]));
-    }
-
-    this_thread::sleep_for(std::chrono::seconds(10));
-    for(auto x:result) std::cout << x << ' ';
-
+    cout << "Feature2 test";
 }
